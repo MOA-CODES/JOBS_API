f2d0c3f1c8d49f7d87998578d718ceefd3d05709
MOA -CODES <moyo.ajla@gmail.com>
2023-07-30 21:26:27 +0100

    tieing a model property to another model

---
+++ models/job_M.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const jobSchema = new mongoose.Schema({
+    company:{
+        type:String,
+        required:[true, 'Please provide company name'],
+        maxlength:50,
+    },
+    position:{
+        type:String,
+        required:[true, 'Please provide position'],
+        maxlength:100,
+    },
+    status:{
+        type:String,
+        enum:['interview', 'declined','pending'],
+        default: 'pending',
+    },
+    createdBy:{
+        type:mongoose.Types.ObjectId,
+        ref:'User',//the model were referencing
+        required:[true, 'Please provide a user']
+    }
+},{timestamps:true})
+
+module.exports = mongoose.model('job', jobSchema)
