c68c38b135de093f8e5bd47bf2c5f113c21645ce
MOA -CODES <moyo.ajla@gmail.com>
2023-08-02 12:29:28 +0100

    update, delete and getsingleJob done

--- controllers/jobs_C.js
+++ controllers/jobs_C.js
@@ -8,7 +8,15 @@ const getAllJobs = async (req, res) => {
 }
 
 const getJob = async (req, res) => {
-    res.send('get job');
+    const {user:{userId}, params:{id:jobId}} = req
+    //const job = await Job.findOne({_id: jobId}).where({createdBy: userId})
+    const job = await Job.findOne({_id: jobId, createdBy: userId})
+
+    if(!job){
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).json({job})
 }
 
 const createJob = async (req, res) => {
@@ -18,11 +26,40 @@ const createJob = async (req, res) => {
 }
 
 const updateJob = async (req, res) => {
-    res.send('update job');
+    const{user:{userId}, params:{id:jobId}, body:{company,position,status}} = req
+
+    let updateStack ={};
+    if(!(company=== '')){
+        updateStack.company = company
+    }if(!(position=== '')){
+        updateStack.position = position
+    }if(status){
+        updateStack.status = status
+    }
+
+    if(!(updateStack.company)|| !(updateStack.position)){
+        throw new BadRequestError('company or position fields cannot be empty')
+    }
+    
+    const job = await Job.findOneAndUpdate({_id:jobId, createdBy: userId}, updateStack, {new:true, runValidators:true})
+
+    if(!job){
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).json({msg:'updated job',updatedJob:job});
 }
 
 const deleteJob = async (req, res) => {
-    res.send('delete job');
+    const {user:{userId}, params:{id:jobId}} = req
+
+    const job = await Job.findByIdAndDelete({_id:jobId, createdBy:userId})
+
+    if(!job){
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).json({msg:`Deleted job with id ${jobId}`});
 }
 
 module.exports = {getAllJobs, getJob, createJob, updateJob, deleteJob}
