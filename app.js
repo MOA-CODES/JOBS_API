5fd8ea76c1dd8b684ae97caf3f8b05403e40507c
MOA -CODES <moyo.ajla@gmail.com>
2023-08-06 13:40:33 +0100

    waiting to deploy

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 //require
 require ('dotenv').config()
 require ('express-async-errors')
+
+//security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const eRateLimit = require('express-rate-limit')
+//
+
 const express = require('express')
 const app = express()
 
@@ -13,10 +21,18 @@ const jobs_R = require('./routes/jobs_R')
 const notfound = require('./middleware/not-found')
 const errorHandler = require('./middleware/error-handler')
 
-
-
 //app.use, functions, and variablles assigning
+app.set('trust proxy', 1);
+app.use(eRateLimit({
+    windowMs: 15 * 60 * 1000, //15minutes
+    max: 100, // limit each Ip to 100 requests per windowMs
+}))
+
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 
 app.use('/api/v1/auth',auth_R )
 app.use('/api/v1/jobs', auth, jobs_R ) //protecting all job routes with authentication middleware
