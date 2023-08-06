8b6e753485e1ec90da9606d786a6ef64722d2464
MOA -CODES <moyo.ajla@gmail.com>
2023-08-06 12:54:42 +0100

    cast error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -13,15 +13,19 @@ const errorHandlerMiddleware = (err, req, res, next) => {
 
   if(err.name === 'ValidationError'){
     customError.msg  = Object.values(err.errors).map((item)=> item.message).join(', ')
-    customError.statusCode = StatusCodes.BAD_REQUEST
+    customError.statusCode = StatusCodes.BAD_REQUEST //400
   }
 
+  if(err.name === 'CastError'){
+    customError.msg=`no item found with id : ${err.value}`
+    customError.statusCode = StatusCodes.NOT_FOUND //404
+  }
 
   if(err.code && err.code === 11000){ //handling duplicate value
     customError.msg= `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
     customError.statusCode = StatusCodes.BAD_REQUEST
   }
-  //return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
   return res.status(customError.statusCode).json({msg:customError.msg })
 
 }
